c9e3a195210e22354cdb758ae7c94f9498377b5c
AnHongjun001 <2637310873@qq.com>
2022-02-08 15:17:47 +0800

    some updates

--- yolo/config.py
+++ yolo/config.py
@@ -13,6 +13,14 @@ from yolo.model.ResNet18_YOLOv1 import ResNet18_YOLOv1
 # project dir
 PROJECT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 
+# check dir
+if not os.path.exists(os.path.join(PROJECT_DIR, "cache")):
+    os.mkdir(os.path.join(PROJECT_DIR, "cache"))
+if not os.path.exists(os.path.join(PROJECT_DIR, "log")):
+    os.mkdir(os.path.join(PROJECT_DIR, "log"))
+if not os.path.exists(os.path.join(PROJECT_DIR, "yolo", "model", "pth")):
+    os.mkdir(os.path.join(PROJECT_DIR, "yolo", "model", "pth"))
+
 # YOLO basic config
 YOLO_DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 YOLO_MODEL = ResNet18_YOLOv1().to(YOLO_DEVICE)
